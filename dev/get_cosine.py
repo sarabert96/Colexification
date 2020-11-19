892dfbee0715e47b9966ed11358491dedac3762f
Sara Bertoldo <55897464+sarabert96@users.noreply.github.com>
2020-11-19 15:12:53 +0100

    Update get_cosine.py
    Added Manage data and fixed functions' calls

--- dev/get_cosine.py
+++ dev/get_cosine.py
@@ -9,14 +9,37 @@ from aux_functions import add_cosine_sim, add_combinations #Sara's functions to 
 df = pd.read_csv('../data/local/df_all_raw.csv') #raw data with single column of concepts
 
 
+### Manage data ###
+# keep only relevant columns
+df = df[['Concepticon_ID','Concepticon_Gloss']].copy()
+# remove duplicates
+df = df.drop_duplicates()
+
+# create column with word count
+df['totalwords'] = df['Concepticon_Gloss'].str.count(' ') + 1
+# delete columns with more than one word
+df = df.drop(df[df.totalwords > 1].index)
+# delete column with word count
+del df['totalwords']
+
+# reset index
+df = df.reset_index(drop=True)
+
+# lowercase
+df['Concepticon_Gloss'] = df['Concepticon_Gloss'].str.lower()
+
+#df=df[:20] #remove comment to reduce size of df 
+
+
 ### Load model ###
 fasttext.util.download_model('en', if_exists='ignore')  # English
 model = FastText.load_fasttext_format('cc.en.300') #load crawl corpus from fasttext
 
 
 ### Process data ###
-comb_df = add_combinations(df)
-cosine_df = add_cosine_sim(comb_df,'column_1','column_2',model)
+comb_df = add_combinations(df,'Concepticon_Gloss','Concepticon_Gloss_2')
+# comb_df['Concepticon_Gloss'][0]='kjhh' # remove comment to insert oov word
+cosine_df = add_cosine_sim(comb_df,'Concepticon_Gloss','Concepticon_Gloss_2',model)
 
 
 ### Write new data frame ###
